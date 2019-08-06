00821cd2b0373fc89a3a0fca473bd1cc5985a01a
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-08-06 23:45:12 +0700

    Exercise-4_Rapi

--- Exercise-4.js
+++ Exercise-4.js
@@ -1,47 +1,35 @@
 function cariModus(arr) {
-    console.log(arr)
- var pengelompokan = [];
- var sementara = [];
- var jumlah = []
-//  console.log(arr)
- for (i=0;i<arr.length;i++){
-    //  console.log(i)
-     if (pengelompokan.length < 1){
-         pengelompokan.push(arr[i])
-         sementara.push([arr[i]])
-        //  console.log(pengelompokan)
-        //  console.log(i)
-     } else {
-         var check = true
-         for (j=0;j<pengelompokan.length;j++){
-             if (pengelompokan[j]===arr[i]){
-                 var index=j
-                 check = false
-             }
-            }
-             if (check){
-                 pengelompokan.push(arr[i])
-                 sementara.push([arr[i]])
-             } else {
-                //  console.log(arr[1])
-                //  console.log(index,'index');
-                 sementara[index].push(arr[i])
-             }
-         
-     }
- }
- if (sementara.length===arr.length || sementara.length === 1){
-     return -1
-    } 
-    console.log(pengelompokan)
-    console.log(sementara);
- jumlah = sementara.sort(function(a,b){return a.length<b.length})
- var modus = sementara[0][0]
- console.log(jumlah)
- return modus;
+  var pengelompokan = [];
+  var sementara = [];
+  for (i = 0; i < arr.length; i++) {
+    if (pengelompokan.length < 1) {
+      pengelompokan.push(arr[i]);
+      sementara.push([arr[i]]);
+    } else {
+      var check = true;
+      for (j = 0; j < pengelompokan.length; j++) {
+        if (pengelompokan[j] === arr[i]) {
+          var index = j;
+          check = false;
+        }
+      }
+      if (check) {
+        pengelompokan.push(arr[i]);
+        sementara.push([arr[i]]);
+      } else {
+        sementara[index].push(arr[i]);
+      }
+    }
+  }
+  if (sementara.length === arr.length || sementara.length === 1) {
+    return -1;
+  }
+  jumlah = sementara.sort(function(a, b) {
+    return a.length < b.length;
+  });
+  var modus = sementara[0][0];
+  return modus;
 }
-
-
 
 // TEST CASES
 console.log(cariModus([10, 4, 5, 2, 4])); // 4
